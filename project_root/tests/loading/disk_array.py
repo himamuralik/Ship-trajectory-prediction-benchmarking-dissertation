ca2eed2b8e3b0945a156a219377b39fc703efc5d
himamuralik <84929346+himamuralik@users.noreply.github.com>
2025-07-23 21:55:49 +0100

    Update disk_array.py

--- project_root/tests/loading/disk_array.py
+++ project_root/tests/loading/disk_array.py
@@ -23,38 +23,51 @@ class DiskArray():
         self.shape = None
         self.nbytes = 0
         self.axis = 0
-        self.static_features = None
-        self.excluded_columns = None  # Track indices of weather/destination columns to exclude
         atexit.register(self.exit_handler, self)
 
-    def set_excluded_columns(self, column_indices):
-        """Set which column indices should be excluded (weather/destination columns)"""
-        self.excluded_columns = column_indices
+    def close(self):
+        """
+        Clear all data
 
-    def attach_static_features(self, static_data):
-        """Associate static features with the DiskArray"""
-        if self.shape is not None and len(static_data) != self.shape[0]:
-            raise ValueError("Static feature count doesn't match main data")
-        self.static_features = static_data
+        Deletes any temp files stored on disk
+
+        :return:
+        """
+        for t in self.temp_paths:
+            t.cleanup()
+        self.temp_paths = []
+        self.shape = None
+        self.nbytes = 0
+        self.axis = 0
+
+    def exit_handler(self, _):
+        """
+        Wrapper for 'close' method, which can be called by atexit
+
+        Used for handling a SIGINT
+
+        :param _:
+        :return:
+        """
+        self.close()
 
     def add_array(self, array):
-        """Add array with column filtering"""
-        if self.excluded_columns is not None:
-            array = self._filter_excluded_columns(array)
+        """
+        Add numpy array to disk array.
+
+        Currently, appended arrays are only added to the 0th axis of the DiskArray
+
+        :param array: Array to append
+        :return:
+        """
         self._update_basic_info(array)
-        
+
         self.temp_paths.append(tempfile.TemporaryDirectory())
+
         self._save_partition_to_path(self.temp_paths[-1].name, array)
         del array
         gc.collect()
 
-    def _filter_excluded_columns(self, data):
-        """Remove excluded columns from data chunks"""
-        if self.excluded_columns is None:
-            return data
-        if isinstance(data, list):
-            return [np.delete(d, self.excluded_columns, axis=1) for d in data]
-        return np.delete(data, self.excluded_columns, axis=1)
 
     def _save_partition_to_path(self, path, data):
         """
@@ -90,6 +103,7 @@ class DiskArray():
             data = data[0]
         return data
 
+
     def __getitem__(self, item):
         """
         Load a chunk by index
@@ -100,24 +114,23 @@ class DiskArray():
         return self._load_partition_from_path(self.temp_paths[item].name)
 
     def compute(self):
-        """Convert to numpy array with static features if available"""
+        """
+        Convert self to numpy array or list of numpy arrays
+
+        Requires that the size of arrays is less than 95% of total system ram
+
+        :return:
+        """
         if self.nbytes > (total_system_ram() * 0.95):
             raise MemoryError('Not enough memory to load the dataset')
-            
         data = [self._load_partition_from_path(t.name) for t in self.temp_paths]
-        
-        # Handle main data concatenation
-        if isinstance(data[0], list):
-            joined_data = [np.concatenate([d[i] for d in data], axis=self.axis) 
-                          for i in range(len(data[0]))]
+        joined_data = []
+        if type(data[0]) == list:
+            for i in range(len(data[0])):
+                joined_data += [np.concatenate([d[i] for d in data], axis=self.axis)]
         else:
             joined_data = np.concatenate(data, axis=self.axis)
-        
-        # Return format depends on static features presence
-        if self.static_features is not None:
-            if isinstance(joined_data, list):
-                return joined_data + [self.static_features]
-            return (joined_data, self.static_features)
+
         return joined_data
 
     def head(self, n):
@@ -158,6 +171,7 @@ class DiskArray():
             raise UserWarning('Dataset does not contain the desired number of records. Entire dataset returned')
 
         return head_data
+
 
     def save_to_disk(self, dir):
         """
@@ -200,6 +214,7 @@ class DiskArray():
             path = os.path.join(dir, p)
             data = self._load_partition_from_path(path)
             self.add_array(data)
+
 
     def __del__(self):
         """
@@ -271,45 +286,25 @@ class DiskArray():
                 self.shape = tuple(self.shape)
 
     def _apply_transformations(self, x_or_y, transformations, normalizer, normalization_factors):
-        """Apply transformations while maintaining static features"""
-        self.nbytes = 0
-        self.shape = None
-        
-        # Save static features temporarily
-        temp_static = self.static_features
-        
-        for t in self.temp_paths:
-            array = self._load_partition_from_path(t.name)
-            array = loading.apply_transformations(
-                array, x_or_y, transformations, 
-                normalizer, normalization_factors
-            )
-            self._update_basic_info(array)
-            self._save_partition_to_path(t.name, array)
-            del array
-            gc.collect()
-        
-        # Restore static features
-        self.static_features = temp_static
-
-    def close(self):
-        """Clean up including static features"""
-        for t in self.temp_paths:
-            t.cleanup()
-        self.temp_paths = []
-        self.shape = None
-        self.nbytes = 0
-        self.axis = 0
-        self.static_features = None
-        self.excluded_columns = None
-
-    def exit_handler(self, _):
         """
-        Wrapper for 'close' method, which can be called by atexit
+        Apply a list of transformations to a DiskArray
 
-        Used for handling a SIGINT
+        Transformations should be a list of specifications generated by a DataLoader, which can be handled by
+        loading.apply_transformations
 
-        :param _:
+        :param x_or_y: Whether this is an input or output dataset
+        :param transformations: List
+        :param normalizer:
+        :param normalization_factors:
         :return:
         """
-        self.close()
+        self.nbytes = 0
+        self.shape = None
+        for t in self.temp_paths:
+            array = self._load_partition_from_path(t.name)
+            array = loading.apply_transformations(array, x_or_y, transformations, normalizer, normalization_factors)
+            self._update_basic_info(array)
+            self._save_partition_to_path(t.name, array)
+
+            del array
+            gc.collect()
