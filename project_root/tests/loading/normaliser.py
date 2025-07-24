cda136591a5acff6415967805f8105e6d836712b
himamuralik <84929346+himamuralik@users.noreply.github.com>
2025-07-24 18:37:02 +0100

    Update normaliser.py

--- project_root/tests/loading/normaliser.py
+++ project_root/tests/loading/normaliser.py
@@ -2,8 +2,7 @@ import numpy as np
 
 from loading.disk_array import DiskArray
 from loading.loading import _find_current_col_idx
-from config import config
-
+from test_config import config
 class Normalizer():
     def __init__(self):
         pass
