61d8243432a3cc6f245d66f9d959e7ac76d116b1
himamuralik <84929346+himamuralik@users.noreply.github.com>
2025-07-24 18:38:12 +0100

    Update median_stopping.py

--- project_root/tests/models/median_stopping.py
+++ project_root/tests/models/median_stopping.py
@@ -5,7 +5,7 @@ from mlflow.entities import ViewType
 import pandas as pd
 import numpy as np
 import os
-from testconfig import config
+from test_config import config
 import urllib
 import keras
 from mlflow import log_metric
